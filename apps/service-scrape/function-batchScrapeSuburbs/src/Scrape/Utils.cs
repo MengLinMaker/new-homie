678e01f6c54ba3fc1e077ae9579a7b76f3ac19db
MengLinMaker <menglinmaker@gmail.com>
2025-02-11 20:26:04 +1100

    chore: just use Log.Logger

---
+++ apps/service-scrape/function-batchScrapeSuburbs/src/Scrape/Utils.cs
@@ -0,0 +1,57 @@
+namespace Scrape;
+
+using HtmlAgilityPack;
+
+using Newtonsoft.Json.Linq;
+
+using Serilog;
+using Serilog.Events;
+
+using SerilogTracing;
+
+<<<<<<< HEAD
+public class Utils
+=======
+public static class Utils
+>>>>>>> 3afe09c (feat: better error handling with with SerilogTracing)
+{
+    /// <summary>
+    /// Extracts JSON data from Next.js sites.
+    /// </summary>
+    /// <returns>
+    /// The JSON JObject or null.
+    /// </returns>
+<<<<<<< HEAD
+    public JObject? ExtractNextJson(string html)
+    {
+        var activity = Log.ForContext<Utils>().StartActivity("ExtractNextJson");
+=======
+    public static JObject? ExtractNextJson(string html)
+    {
+<<<<<<< HEAD
+        var activity = Log.ForContext<JObject>().StartActivity("Scrape.Utils.ExtractNextJson");
+>>>>>>> 3afe09c (feat: better error handling with with SerilogTracing)
+=======
+        var activity = Log.Logger.StartActivity("Scrape.Utils.ExtractNextJson");
+>>>>>>> 544851d (chore: just use Log.Logger)
+        try
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var nextDataScriptNode = htmlDocument.DocumentNode.SelectSingleNode("//script[@id='__NEXT_DATA__']");
+            if (nextDataScriptNode == null)
+            {
+                activity.Complete(LogEventLevel.Warning, new ArgumentException("Next.js JSON not found"));
+                return null;
+            }
+            var jsonText = nextDataScriptNode.InnerText;
+            activity.Complete();
+            return JObject.Parse(jsonText);
+        }
+        catch (Exception ex)
+        {
+            activity.Complete(LogEventLevel.Warning, ex);
+            return null;
+        }
+    }
+}
