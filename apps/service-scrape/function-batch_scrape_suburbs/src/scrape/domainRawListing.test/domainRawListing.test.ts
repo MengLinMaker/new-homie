1f7ebf7d09ea8aaaca2e15e85cad886f2c4509a4
MengLinMaker <menglinmaker@gmail.com>
2025-02-25 02:51:55 +1100

    test: add for each test file config

--- apps/service-scrape/function-batch_scrape_suburbs/src/scrape/domainRawListing.test/domainRawListing.test.ts
+++ apps/service-scrape/function-batch_scrape_suburbs/src/scrape/domainRawListing.test/domainRawListing.test.ts
@@ -3,28 +3,33 @@ import { domainRawListing } from '../domainRawListing'
 import { readFileSync } from 'node:fs'
 
 describe('domainRawListing', () => {
-  describe('tryExtractListings', () => {
-    it('should extract listings successfully', async () => {
-      const inputFile = 'raw.rent.dandenong-vic-3175.json'
-      const expectedFile = 'tryExtractListings.rent.dandenong-vic-3175.json'
-      const inputObject = JSON.parse(readFileSync(`${import.meta.dirname}/${inputFile}`).toString())
-      const expectedObject = JSON.parse(
-        readFileSync(`${import.meta.dirname}/${expectedFile}`).toString(),
-      )
+  describe.for(['rent.dandenong-vic-3175', 'sale.dandenong-vic-3175'])(
+    'tryExtractListings from %s',
+    (fileSuffix) => {
+      it('should extract listings successfully', async () => {
+        const inputFile = `raw.${fileSuffix}.json`
+        const expectedFile = `tryExtractListings.${fileSuffix}.json`
+        const inputObject = JSON.parse(
+          readFileSync(`${import.meta.dirname}/${inputFile}`).toString(),
+        )
+        const expectedObject = JSON.parse(
+          readFileSync(`${import.meta.dirname}/${expectedFile}`).toString(),
+        )
 
-      const [value, success] = await domainRawListing.tryExtractListings(inputObject)
-      if (!success) {
-        expect(success).toBe(true)
-        return
-      }
-      const [resultObject, _isLastpage] = value
-      expect(resultObject).toStrictEqual(expectedObject)
-    })
+        const [value, success] = await domainRawListing.tryExtractListings(inputObject)
+        if (!success) {
+          expect(success).toBe(true)
+          return
+        }
+        const [resultObject, _isLastpage] = value
+        expect(resultObject).toStrictEqual(expectedObject)
+      })
 
-    it('should not extract invalid input', async () => {
-      const [resultObject, success] = await domainRawListing.tryExtractListings({})
-      expect(success).toBe(false)
-      expect(resultObject).toBeInstanceOf(Error)
-    })
-  })
+      it('should not extract invalid input', async () => {
+        const [resultObject, success] = await domainRawListing.tryExtractListings({})
+        expect(success).toBe(false)
+        expect(resultObject).toBeInstanceOf(Error)
+      })
+    },
+  )
 })
