2e91e584550de22f2017463e9f06a8e8cf621284
Meng Lin <menglinmaker@gmail.com>
2025-08-03 21:41:24 +1000

    chore: format

--- apps/service-scrape/Function_BatchScrapeSuburbs/src/main/java/org/scrape/generated/domain/RawListingsSchema/Features.java
+++ apps/service-scrape/Function_BatchScrapeSuburbs/src/main/java/org/scrape/generated/domain/RawListingsSchema/Features.java
@@ -1,43 +1,41 @@
-
 package org.scrape.generated.domain.RawListingsSchema;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import javax.annotation.processing.Generated;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "beds",
-    "baths",
-    "parking",
-    "propertyType",
-    "isRural",
-    "landSize",
-    "isRetirement"
-})
+@JsonPropertyOrder({"beds", "baths", "parking", "propertyType", "isRural", "landSize", "isRetirement"})
 @Generated("jsonschema2pojo")
 public class Features {
 
     @JsonProperty("beds")
     public Integer beds;
+
     @JsonProperty("baths")
     public Integer baths;
+
     @JsonProperty("parking")
     public Integer parking;
+
     @JsonProperty("propertyType")
     public String propertyType;
+
     @JsonProperty("isRural")
     public Boolean isRural;
+
     @JsonProperty("landSize")
     public Integer landSize;
+
     @JsonProperty("isRetirement")
     public Boolean isRetirement;
+
     @JsonIgnore
     private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
 
@@ -50,5 +48,4 @@ public class Features {
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
     }
-
 }
--- apps/service-scrape/Function_BatchScrapeSuburbs/src/main/java/org/scrape/generated/domain/RawListingsSchema/ListingModel.java
+++ apps/service-scrape/Function_BatchScrapeSuburbs/src/main/java/org/scrape/generated/domain/RawListingsSchema/ListingModel.java
@@ -1,40 +1,38 @@
-
 package org.scrape.generated.domain.RawListingsSchema;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import javax.annotation.processing.Generated;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "url",
-    "price",
-    "address",
-    "features",
-    "inspection",
-    "auction"
-})
+@JsonPropertyOrder({"url", "price", "address", "features", "inspection", "auction"})
 @Generated("jsonschema2pojo")
 public class ListingModel {
 
     @JsonProperty("url")
     public String url;
+
     @JsonProperty("price")
     public String price;
+
     @JsonProperty("address")
     public Address address;
+
     @JsonProperty("features")
     public Features features;
+
     @JsonProperty("inspection")
     public Inspection inspection;
+
     @JsonProperty("auction")
     public String auction;
+
     @JsonIgnore
     private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
 
@@ -47,5 +45,4 @@ public class ListingModel {
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
     }
-
 }
