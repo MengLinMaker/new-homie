91b0784b8f3e52733b1c1e18d604446a37ae6a7d
Meng Lin <menglinmaker@gmail.com>
2025-08-08 15:21:21 +1000

    feat: finalise migration failed

--- apps/service-scrape/lib-db_service_scrape/migrate.ts
+++ apps/service-scrape/lib-db_service_scrape/migrate.ts
@@ -1,5 +1,10 @@
 import { DB_SERVICE_SCRAPE } from './src/index.ts'
+import { LOG } from './src/log.ts'
 import { kyselyPostgresMigrate } from './src/migrator.ts'
 
 const db = await kyselyPostgresMigrate(DB_SERVICE_SCRAPE)
-await db.destroy()
+if (db === null) {
+    LOG.fatal('MIGRATION FAILED')
+} else {
+    await db.destroy()
+}
