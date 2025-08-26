732807c3192cc8fd5bae8fedc43bb9e282c64e3f
Meng Lin <menglinmaker@gmail.com>
2025-08-26 12:29:06 +1000

    test: test configs

---
+++ apps/observability/lib-opentelemetry/vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        pool: 'threads',
+        reporters: ['verbose', 'github-actions'],
+        logHeapUsage: true,
+        coverage: {
+            provider: 'v8',
+            reporter: ['text'],
+            exclude: ['src/util/commitId.ts', 'src/startOpenTelemetry.ts', 'src/index.ts'],
+            include: ['src/**/*.ts'],
+        },
+    },
+})
