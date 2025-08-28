b7541feccb7267a93f429e3456aaff050bfa8c8b
Meng Lin <menglinmaker@gmail.com>
2025-08-28 22:45:50 +1000

    fix: enable __dirname and __filename with esbuild banner

--- apps/service-scrape/cdk/src/util/functionDefaults.ts
+++ apps/service-scrape/cdk/src/util/functionDefaults.ts
@@ -15,8 +15,17 @@ export const NODE_OPTIONS = '--enable-source-maps'
  */
 const bundling: BundlingOptions = {
     format: OutputFormat.ESM,
-    // Banner to fix esbuild dynamic require bug - https://github.com/evanw/esbuild/issues/1944#issuecomment-1022886747
-    banner: `import { createRequire } from 'module'; const require = createRequire(import.meta.url)`,
+    // lines joined to prevent syntax token error
+    banner: [
+        // Banner to enable dynamic require in esm - https://github.com/evanw/esbuild/issues/1944#issuecomment-1022886747
+        `import { createRequire } from 'module'`,
+        `const require = createRequire(import.meta.url)`,
+        // Banner to enable __filename and __dirname - https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+        `import { fileURLToPath } from 'node:url'`,
+        `import { dirname } from 'node:path'`,
+        `const __filename = fileURLToPath(import.meta.url)`,
+        `const __dirname = dirname(__filename)`,
+    ].join(';'),
     // Minify with names for logging purposes
     minify: true,
     keepNames: true,
