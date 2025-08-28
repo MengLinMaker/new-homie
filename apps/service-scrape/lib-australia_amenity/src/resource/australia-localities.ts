85cc9092f51e001a639414cd968510161bf9ad64
Meng Lin <menglinmaker@gmail.com>
2025-08-28 17:06:45 +1000

    chore: remove unused files

--- apps/service-scrape/lib-australia_amenity/src/resource/australia-localities.ts
+++
@@ -1,5 +0,0 @@
-import _australiaLocalities from './australia-localities.json'
-import type { Updateable } from 'kysely'
-import type { Schema } from '@service-scrape/lib-db_service_scrape'
-
-export const australiaLocalities = _australiaLocalities as Updateable<Schema.LocalityTable>[]
--- apps/service-scrape/lib-australia_amenity/src/resource/australia-schools.ts
+++
@@ -1,9 +0,0 @@
-import _australiaSchools from './australia-schools.json'
-import type { Updateable } from 'kysely'
-import type { Schema } from '@service-scrape/lib-db_service_scrape'
-
-export const australiaSchools = _australiaSchools as {
-    school_table: Updateable<Schema.SchoolTable>
-    school_feature_table: Updateable<Schema.SchoolFeatureTable>
-    locality_table: Updateable<Schema.LocalityTable>
-}[]
