ca17340e97ebf90d05dd529c8f266dfe420791a4
Meng Lin <menglinmaker@gmail.com>
2025-09-04 13:59:19 +1000

    chore: export otel ENV for cdk

--- apps/observability/lib-opentelemetry/src/index.ts
+++ apps/observability/lib-opentelemetry/src/index.ts
@@ -1,7 +1,8 @@
+import { ENV } from './env'
 import { ILoggable } from './ILoggable'
 import { startOpenTelemetry } from './startOpenTelemetry'
 import { LOGGER, otelException } from './logger'
 import { commitId } from './commitId'
 import { parseEnvSchema } from './parseEnvSchema'
 
-export { startOpenTelemetry, LOGGER, ILoggable, otelException, commitId, parseEnvSchema }
+export { startOpenTelemetry, LOGGER, ILoggable, otelException, commitId, parseEnvSchema, ENV }
