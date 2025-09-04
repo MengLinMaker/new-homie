dca6703d3d2cae9ce3df0bda951aa9c7bd7f8b2f
Meng Lin <menglinmaker@gmail.com>
2025-09-04 14:02:21 +1000

    fix: boot chrome in AWS Lambda from S3 to overcome file permissions

--- apps/service-scrape/function-scrape_locality/src/scrape/website/BrowserService.ts
+++ apps/service-scrape/function-scrape_locality/src/scrape/website/BrowserService.ts
@@ -1,7 +1,11 @@
 import type { Logger } from 'pino'
-import { ILoggable } from '@observability/lib-opentelemetry'
-import { launchChromium } from 'playwright-aws-lambda'
-import type { Browser, BrowserContext } from 'playwright'
+import { ILoggable, LOGGER, otelException } from '@observability/lib-opentelemetry'
+import puppeteer, { Browser, BrowserContext } from 'puppeteer-core'
+import chromium from '@sparticuz/chromium-min'
+import { existsSync } from 'node:fs'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { URIError } from 'common-errors'
 
 export class BrowserService extends ILoggable {
     static browser: Browser
@@ -13,14 +17,54 @@ export class BrowserService extends ILoggable {
     }
 
     /* v8 ignore start */
+    static async launchBrowser() {
+        if (BrowserService.browser) return BrowserService.browser
+
+        // Local chrome execution
+        if (existsSync('./.puppeteer'))
+            return await puppeteer.launch({
+                args: chromium.args,
+                executablePath: './.puppeteer/chrome-headless-shell',
+            })
+
+        // Download chrome at runtime to increase deployment speed
+        const s3Client = new S3Client()
+        const chromeTarUrl = process.env['CHROME_PUPPETEER_ASSET_URL']
+        if (!chromeTarUrl) {
+            const error = new URIError('CHROME_PUPPETEER_ASSET_URL is undefined')
+            LOGGER.fatal(otelException(error))
+            throw error
+        }
+        const [, bucket, key] = new URL(chromeTarUrl).pathname.split('/')
+        const chromeTarSignedUrl = await getSignedUrl(
+            s3Client,
+            new GetObjectCommand({
+                Bucket: bucket,
+                Key: key,
+            }),
+            { expiresIn: 60 },
+        )
+        return await puppeteer.launch({
+            args: chromium.args,
+            executablePath: await chromium.executablePath(chromeTarSignedUrl),
+        })
+    }
+
+    /* v8 ignore start */
     static async createBrowserContext() {
-        const browser = BrowserService.browser ?? (await launchChromium({ headless: true }))
-        const browserContexts = browser.contexts()
-        return browserContexts[0] ?? browser.newContext()
+        const browser = await BrowserService.launchBrowser()
+        BrowserService.browser = browser
+        const browserContexts = browser.browserContexts()
+        return browserContexts[0] ?? browser.createBrowserContext()
+    }
+
+    /* v8 ignore start */
+    static async close() {
+        BrowserService.browser.close()
     }
 
     private async singlePage() {
-        const pages = this.browserContext.pages()
+        const pages = await this.browserContext.pages()
         return pages[0] ?? (await this.browserContext.newPage())
     }
 
