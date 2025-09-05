3ca76a65e14ffea05a4c6fc8f7cbdc4410357563
Meng Lin <menglinmaker@gmail.com>
2025-09-06 02:30:52 +1000

    fix: test for lambda

--- apps/service-scrape/function-scrape_locality/test/handler.test.ts
+++ apps/service-scrape/function-scrape_locality/test/handler.test.ts
@@ -1,11 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { suiteNameFromFileName } from './util'
-import { handler } from '../src/index.mts'
 import { StatusCodes } from 'http-status-codes'
 
 const testSuiteName = suiteNameFromFileName(import.meta.filename)
 
 describe(testSuiteName, async () => {
+    // biome-ignore lint/complexity/useLiteralKeys: <setting value>
+    process.env['CHROME_PUPPETEER_ASSET_URL'] = '../../asset/chromium-v138.0.2-pack.arm64.tar'
+    const { handler } = await import('../src/index.mts')
+
     it('Should validate incorrect input', async () => {
         const result = await handler({} as never, undefined as never)
         expect(result).toStrictEqual({ status: StatusCodes.BAD_REQUEST })
