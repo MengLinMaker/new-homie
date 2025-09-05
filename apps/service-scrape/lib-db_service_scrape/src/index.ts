9a55c36deb762c719fee769d2a5d1d4e393ed991
Meng Lin <menglinmaker@gmail.com>
2025-09-05 15:16:55 +1000

    chore: init test database url

--- apps/service-scrape/lib-db_service_scrape/src/index.ts
+++ apps/service-scrape/lib-db_service_scrape/src/index.ts
@@ -9,8 +9,9 @@ config({ quiet: true })
  * Uri to database from env variable
  * @todo: Replace env variable with SSM parameter store for better security
  */
-// biome-ignore lint/complexity/useLiteralKeys: <untyped>
-export const DB_SERVICE_SCRAPE = process.env['DB_SERVICE_SCRAPE']
+export const DB_SERVICE_SCRAPE =
+    // biome-ignore lint/complexity/useLiteralKeys: <untyped>
+    process.env['DB_SERVICE_SCRAPE'] ?? 'postgres://user:password@localhost:54320/db'
 
 export {
     getKyselyPostgresDb,
