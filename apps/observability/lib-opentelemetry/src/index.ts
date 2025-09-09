d34a41de22c34cb1e5382c21a7880ba7f1653f57
Meng Lin <menglinmaker@gmail.com>
2025-09-09 21:18:37 +1000

    generate commitId to dist folder for build

--- apps/observability/lib-opentelemetry/src/index.ts
+++ apps/observability/lib-opentelemetry/src/index.ts
@@ -1,8 +1,8 @@
+import { commitId } from './../dist/commitId.ts'
 import { ENV } from './env.ts'
 import { ILoggable } from './ILoggable.ts'
 import { OpenTelemetry, spanExceptionEnd } from './OpenTelemetry.ts'
 import { otelException, enforceErrorType } from './otelException.ts'
-import { commitId } from './commitId.ts'
 import { parseEnvSchema } from './parseEnvSchema.ts'
 
 export {
