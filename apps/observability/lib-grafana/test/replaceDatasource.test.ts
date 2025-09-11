2d02e62ebd107128dd32a73b72042a6a744258da
Meng Lin <menglinmaker@gmail.com>
2025-09-12 03:41:29 +1000

    fix test

--- apps/observability/lib-grafana/test/replaceDatasource.test.ts
+++ apps/observability/lib-grafana/test/replaceDatasource.test.ts
@@ -1,13 +1,15 @@
 import { describe, it, expect } from 'vitest'
 import { readJsonFile } from '../src/util'
-import { replaceDatasource } from '../src/replaceDatasource'
+import { DashboardModifier } from '../src/replaceDatasource'
 import path from 'node:path'
 
 describe('replaceDatasource', () => {
+    const dashboardModifier = new DashboardModifier()
+
     it('should replace datasource', () => {
         const input = readJsonFile(path.join(__dirname, './in.dashboard.json'))
         const expected = readJsonFile(path.join(__dirname, './out.dashboard.json'))
-        const { output } = replaceDatasource(input, [
+        const { output } = dashboardModifier.replaceDatasource(input, [
             {
                 old: { uid: 'loki' },
                 new: { type: 'loki', uid: 'grafanacloud-logs' },
