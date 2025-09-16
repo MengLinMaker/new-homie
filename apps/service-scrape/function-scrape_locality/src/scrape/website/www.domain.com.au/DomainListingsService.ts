8ef9ff45d4e5e5e64d99a7a37d88537abab1923b
Meng Lin <menglinmaker@gmail.com>
2025-09-16 11:22:55 +1000

    ignore warning if it contains a time or date

--- apps/service-scrape/function-scrape_locality/src/scrape/website/www.domain.com.au/DomainListingsService.ts
+++ apps/service-scrape/function-scrape_locality/src/scrape/website/www.domain.com.au/DomainListingsService.ts
@@ -72,9 +72,13 @@ const nextDataJsonSchema = z.object({
 export class DomainListingsService extends ILoggable {
     expectedNoMatchingPrice(priceString: string) {
         // No number to extract
-        if (!priceString.match(/\d/g)) return true
+        if (!priceString.match(/\d/)) return true
         // Requires agent contact
-        if (!priceString.match(/(auction|call|contact|offer|expression)/gi)) return true
+        if (!priceString.match(/(auction|call|contact|offer|expression)/i)) return true
+        // Contains a time
+        if (!priceString.match(/\d{1,2}(:\d{1,2})?(pm|am)/)) return true
+        // Contains a date
+        if (!priceString.match(/\b\d{2}\b(\/|-)\b\d{2}\b(\/|-)\b\d{2,4}\b/)) return true
         return false
     }
 
