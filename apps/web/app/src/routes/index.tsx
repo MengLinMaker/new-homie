f63dd96c1e6696ddba1d48cee358b8549de96755
Meng Lin <menglinmaker@gmail.com>
2025-09-19 02:06:11 +1000

    placeholder landing page

--- apps/web/app/src/routes/index.tsx
+++ apps/web/app/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 export const Route = createFileRoute('/')({
     component: App,
@@ -9,28 +8,7 @@ function App() {
     return (
         <div className="text-center">
             <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <a
-                    className="text-[#61dafb] hover:underline"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="text-[#61dafb] hover:underline"
-                    href="https://tanstack.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn TanStack
-                </a>
+                hello
             </header>
         </div>
     )
