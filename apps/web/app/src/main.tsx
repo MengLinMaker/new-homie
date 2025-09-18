2ccdf4f9c4e3c04cdc377d3b8ce37ad39493e926
Meng Lin <menglinmaker@gmail.com>
2025-09-18 16:55:17 +1000

    clerk component

--- apps/web/app/src/main.tsx
+++ apps/web/app/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
@@ -7,6 +7,10 @@ import { routeTree } from './routeTree.gen'
 
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!CLERK_PUBLISHABLE_KEY) throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY')
 
 // Create a new router instance
 const router = createRouter({
@@ -25,13 +29,14 @@ declare module '@tanstack/react-router' {
     }
 }
 
-// Render the app
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
-    const root = ReactDOM.createRoot(rootElement)
+    const root = createRoot(rootElement)
     root.render(
         <StrictMode>
-            <RouterProvider router={router} />
+            <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+                <RouterProvider router={router} />
+            </ClerkProvider>
         </StrictMode>,
     )
 }
@@ -39,4 +44,4 @@ if (rootElement && !rootElement.innerHTML) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals(console.log)
