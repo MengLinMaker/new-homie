a6d0198013960c1e4f4eb693ea58438149983a25
Meng Lin <menglinmaker@gmail.com>
2025-09-26 14:24:53 +1000

    separate queries

--- apps/service-scrape/lib-db_service_scrape/src/migration/V0011__add-latest_sale_view.ts
+++ apps/service-scrape/lib-db_service_scrape/src/migration/V0011__add-latest_sale_view.ts
@@ -2,56 +2,45 @@ import type { Kysely } from 'kysely'
 import type { DB } from '../schema.ts'
 
 export async function up(db: Kysely<DB>) {
-    await db.schema
-        .createView('latest_sale_view')
-        .as(
-            db
-                .selectFrom('sale_price_table')
-                .leftJoin('home_table', 'sale_price_table.home_table_id', 'home_table.id')
-                .leftJoin(
-                    'home_feature_table',
-                    'home_table.home_feature_table_id',
-                    'home_feature_table.id',
-                )
-                .leftJoin('locality_table', 'home_table.locality_table_id', 'locality_table.id')
-                .select([
-                    // sale_price_table columns
-                    'first_scrape_date',
-                    'last_scrape_date',
-                    'higher_price_aud',
-                    'aud_per_bed',
-                    'aud_per_land_m2',
-                    // home_table columns
-                    'home_table_id',
-                    'street_address',
-                    'gps',
-                    'land_m2',
-                    'inspection_time',
-                    'auction_time',
-                    // home_feature_table columns
-                    'home_feature_table_id',
-                    'suburb_name',
-                    'state_abbreviation',
-                    'postcode',
-                    'boundary_coordinates',
-                    // locality_table columns
-                    'locality_table_id',
-                    'bed_quantity',
-                    'bath_quantity',
-                    'car_quantity',
-                    'home_type',
-                    'is_retirement',
-                ])
-                .where(
-                    'last_scrape_date',
-                    '=',
-                    db
-                        .selectFrom('sale_price_table')
-                        // @ts-expect-error: Kysely bug with aggregate functions in subqueries
-                        .select(({ fn }) => fn.max('last_scrape_date')),
-                ),
-        )
-        .execute()
+    const highest_last_scrape_date = db
+        .selectFrom('sale_price_table')
+        // @ts-expect-error: Kysely bug with aggregate functions in subqueries
+        .select(({ fn }) => fn.max('last_scrape_date'))
+    const latest_sale_view = db
+        .selectFrom('sale_price_table')
+        .leftJoin('home_table', 'sale_price_table.home_table_id', 'home_table.id')
+        .leftJoin('home_feature_table', 'home_table.home_feature_table_id', 'home_feature_table.id')
+        .leftJoin('locality_table', 'home_table.locality_table_id', 'locality_table.id')
+        .select([
+            // sale_price_table columns
+            'first_scrape_date',
+            'last_scrape_date',
+            'higher_price_aud',
+            'aud_per_bed',
+            'aud_per_land_m2',
+            // home_table columns
+            'home_table_id',
+            'street_address',
+            'gps',
+            'land_m2',
+            'inspection_time',
+            'auction_time',
+            // home_feature_table columns
+            'home_feature_table_id',
+            'suburb_name',
+            'state_abbreviation',
+            'postcode',
+            'boundary_coordinates',
+            // locality_table columns
+            'locality_table_id',
+            'bed_quantity',
+            'bath_quantity',
+            'car_quantity',
+            'home_type',
+            'is_retirement',
+        ])
+        .where('last_scrape_date', '=', highest_last_scrape_date)
+    await db.schema.createView('latest_sale_view').as(latest_sale_view).execute()
 }
 
 export async function down(db: Kysely<DB>) {
