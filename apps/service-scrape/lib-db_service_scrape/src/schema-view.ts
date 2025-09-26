b4c05ddd578062ed1a005e5d90bd3d5516538988
Meng Lin <menglinmaker@gmail.com>
2025-09-26 14:20:21 +1000

    manually copy schema for view

--- apps/service-scrape/lib-db_service_scrape/src/schema-view.ts
+++ apps/service-scrape/lib-db_service_scrape/src/schema-view.ts
@@ -1,27 +1,31 @@
-import type { HomeTypeEnum, StateAbbreviationEnum, Timestamp } from './schema'
+import type { Generated, HomeTypeEnum, StateAbbreviationEnum, Timestamp } from './schema'
 
 export interface LatestSaleView {
-    auction_time: Timestamp | null
+    // sale_price_table columns
     aud_per_bed: number
     aud_per_land_m2: number
+    first_scrape_date: Generated<Timestamp>
+    higher_price_aud: number
+    home_table_id: number
+    last_scrape_date: Generated<Timestamp>
+    // home_table columns
+    auction_time: Timestamp | null
+    gps: string | null
+    home_feature_table_id: number
+    inspection_time: Timestamp | null
+    land_m2: number
+    locality_table_id: number
+    street_address: string
+    // home_feature_table columns
     bath_quantity: number
     bed_quantity: number
-    boundary_coordinates: string
     car_quantity: number
-    first_scrape_date: Timestamp
-    gps: string | null
-    higher_price_aud: number
-    home_feature_table_id: number
-    home_table_id: number
     home_type: HomeTypeEnum
-    inspection_time: Timestamp | null
     is_retirement: boolean
-    land_m2: number
-    last_scrape_date: Timestamp
-    locality_table_id: number
+    // locality_table columns
+    boundary_coordinates: string
     postcode: string
     state_abbreviation: StateAbbreviationEnum
-    street_address: string
     suburb_name: string
 }
 
