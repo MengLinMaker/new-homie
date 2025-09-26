fc08459e122881b69e8b9e944adcd8dff32c4e3d
Meng Lin <menglinmaker@gmail.com>
2025-09-26 23:00:44 +1000

    fix filter errors type

--- apps/service-scrape/function-scrape_locality/src/scrape/website/www.domain.com.au/DomainListingsService.ts
+++ apps/service-scrape/function-scrape_locality/src/scrape/website/www.domain.com.au/DomainListingsService.ts
@@ -77,11 +77,11 @@ export class DomainListingsService extends ILoggable {
         // No number to extract
         if (!priceString.match(/\d/i)) return true
         // Requires agent contact
-        if (!priceString.match(/(auction|call|contact|offer|expression)/i)) return true
+        if (priceString.match(/(auction|call|contact|offer|expression)/i)) return true
         // Contains a time
-        if (!priceString.match(/\d{1,2}(:\d{1,2})?(pm|am)/i)) return true
+        if (priceString.match(/\d{1,2}(:\d{1,2})?(pm|am)/i)) return true
         // Contains a date
-        if (!priceString.match(/\b\d{2}\b(\/|-)\b\d{2}\b(\/|-)\b\d{2,4}\b/i)) return true
+        if (priceString.match(/\b\d{2}\b(\/|-)\b\d{2}\b(\/|-)\b\d{2,4}\b/i)) return true
         return false
     }
 
