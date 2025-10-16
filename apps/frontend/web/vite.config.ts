d875ff6932979e9484a66ed33dcba0c8fec964b1
Meng Lin <menglinmaker@gmail.com>
2025-10-16 14:11:46 +1100

    add auto updating service worker

--- apps/frontend/web/vite.config.ts
+++ apps/frontend/web/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 export default defineConfig({
-    plugins: [tanstackRouter({ autoCodeSplitting: true }), react(), tailwindcss()],
+    plugins: [
+        tanstackRouter({ autoCodeSplitting: true }),
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+        }),
+    ],
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
