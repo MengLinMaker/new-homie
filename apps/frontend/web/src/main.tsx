dd925c7b62eefe7fffdc6844ccb96f5f4e4f7a69
Meng Lin <menglinmaker@gmail.com>
2025-11-27 17:42:32 +1100

    query client

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -8,6 +8,7 @@ import { routeTree } from './routeTree.gen'
 import './styles.css'
 import './clerk.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!CLERK_PUBLISHABLE_KEY) throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY')
@@ -29,14 +30,18 @@ declare module '@tanstack/react-router' {
     }
 }
 
+const queryClient = new QueryClient()
+
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
     const root = createRoot(rootElement)
     root.render(
         <StrictMode>
-            <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-                <RouterProvider router={router} />
-            </ClerkProvider>
+            <QueryClientProvider client={queryClient}>
+                <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+                    <RouterProvider router={router} />
+                </ClerkProvider>
+            </QueryClientProvider>
         </StrictMode>,
     )
 }
