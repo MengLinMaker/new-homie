eb746ab3fd9fb67e325f7abc4f945f3bb5bc5db5
Meng Lin <menglinmaker@gmail.com>
2025-11-27 19:52:41 +1100

    deploy api gateway

--- apps/service-scrape/infra-service_scrape_query.ts
+++ apps/service-scrape/infra-service_scrape_query.ts
@@ -1,23 +1,23 @@
-// /// <reference path="../../.sst/platform/config.d.ts" />
+/// <reference path="../../.sst/platform/config.d.ts" />
 
-// import { ApiGatewayV2, OTEL_ENV } from '../infra-common'
-// import { basePath } from './function-query_freemium/src'
-// import { DB_SERVICE_SCRAPE } from './lib-db_service_scrape/src/index'
-// import path from 'node:path'
+import { ApiGatewayV2, OTEL_ENV } from '../infra-common'
+import { basePath } from './function-query_freemium/src'
+import { DB_SERVICE_SCRAPE } from './lib-db_service_scrape/src/index'
+import path from 'node:path'
 
-// const dirname = './apps/service-scrape'
+const dirname = './apps/service-scrape'
 
-// ApiGatewayV2.route(
-//     `ANY ${basePath}/{proxy+}`,
-//     {
-//         handler: path.join(dirname, './function-query_freemium/src/index.handler'),
-//         architecture: 'arm64',
-//         runtime: 'nodejs22.x',
-//         timeout: '3 seconds',
-//         concurrency: { reserved: 10 },
-//         environment: { ...OTEL_ENV, DB_SERVICE_SCRAPE },
-//     },
-//     // {
-//     //     auth: { iam: true },
-//     // },
-// )
+ApiGatewayV2.route(
+    `ANY ${basePath}/{proxy+}`,
+    {
+        handler: path.join(dirname, './function-query_freemium/src/index.handler'),
+        architecture: 'arm64',
+        runtime: 'nodejs22.x',
+        timeout: '3 seconds',
+        concurrency: { reserved: 10 },
+        environment: { ...OTEL_ENV, DB_SERVICE_SCRAPE },
+    },
+    // {
+    //     auth: { iam: true },
+    // },
+)
