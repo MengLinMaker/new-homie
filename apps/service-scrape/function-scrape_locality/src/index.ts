a68f0302b177a792c7d96ad166725f4d90bfb343
Meng Lin <menglinmaker@gmail.com>
2025-11-28 01:36:18 +1100

    log progress task

--- apps/service-scrape/function-scrape_locality/src/index.ts
+++ apps/service-scrape/function-scrape_locality/src/index.ts
@@ -2,4 +2,5 @@ import { parseEnvSchema } from '@observability/lib-opentelemetry'
 import { handler, handlerSchema } from './handler'
 
 const locality = await parseEnvSchema(handlerSchema)
+console.info('Scraping locality:', JSON.stringify(locality))
 await handler(locality)
