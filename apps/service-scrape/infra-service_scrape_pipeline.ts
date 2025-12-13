34b0f9895e910cf8bcec6a4b945e03ad78445e1c
Meng Lin <menglinmaker@gmail.com>
2025-12-13 19:36:22 +1100

    add working aws batch job

--- apps/service-scrape/infra-service_scrape_pipeline.ts
+++ apps/service-scrape/infra-service_scrape_pipeline.ts
@@ -1,18 +1,77 @@
 /// <reference path="../../.sst/platform/config.d.ts" />
 
-import { OTEL_ENV } from '../infra-common'
+import { OTEL_ENV, Repository } from '../infra-common'
 import { DB_SERVICE_SCRAPE } from './lib-db_service_scrape/src/index'
 import path from 'node:path'
+import * as pulumi from '@pulumi/pulumi'
+import { RoleBatchEcs } from './infra-aws_batch_roles'
 
 const dirname = './apps/service-scrape'
+
+/**
+ * Expands key value map for AWS format - https://docs.aws.amazon.com/batch/latest/APIReference/API_KeyValuePair.html
+ * @param envMap key value map
+ */
+const expandEnv = (envMap: { [k: string]: string }) => {
+    const expanded: { name: string; value: string }[] = []
+    for (const [name, value] of Object.entries(envMap)) expanded.push({ name, value })
+    return expanded
+}
+
+/**
+ * 3. AWS Batch for fault tolerant scraping on cheap fargate spot instances
+ */
+const Vpc = new sst.aws.Vpc('Vpc', { az: 3 })
+const ComputeEnvironment = new aws.batch.ComputeEnvironment('ComputeEnvironment', {
+    computeResources: {
+        maxVcpus: 1,
+        securityGroupIds: Vpc.securityGroups,
+        subnets: Vpc.publicSubnets,
+        type: 'FARGATE_SPOT',
+    },
+    type: 'MANAGED',
+})
+const ImageScrapeLocality = new awsx.ecr.Image('ImageScrapeLocality', {
+    repositoryUrl: Repository.url,
+    context: path.join('../../', dirname, './function-scrape_locality'),
+})
+const JobDefinitionScrapeLocality = new aws.batch.JobDefinition('JobDefinitionScrapeLocality', {
+    type: 'container',
+    platformCapabilities: ['FARGATE'],
+    timeout: {
+        attemptDurationSeconds: 60 * 30,
+    },
+    containerProperties: pulumi.jsonStringify({
+        executionRoleArn: RoleBatchEcs.arn,
+        image: ImageScrapeLocality.imageUri,
+        runtimePlatform: { cpuArchitecture: 'ARM64' },
+        command: ['node', '/app/index.js'],
+        resourceRequirements: [
+            { type: 'VCPU', value: '1' },
+            { type: 'MEMORY', value: '2048' },
+        ],
+        networkConfiguration: { assignPublicIp: 'ENABLED' },
+        environment: expandEnv({
+            DB_SERVICE_SCRAPE,
+            ...OTEL_ENV,
+            // Default testing inputs
+            suburb_name: 'Test',
+            state_abbreviation: 'VIC',
+            postcode: '0000',
+        }),
+    }),
+})
+const JobQueueScrapeLocality = new aws.batch.JobQueue('JobQueueScrapeLocality', {
+    state: 'ENABLED',
+    priority: 1,
+    computeEnvironmentOrders: [{ computeEnvironment: ComputeEnvironment.arn, order: 1 }],
+})
 
 /**
  * 3. ECS Fargate task to run the scrape job
  */
 const ScrapeLocalityTask = new sst.aws.Task('ScrapeLocalityTask', {
-    cluster: new sst.aws.Cluster('ScrapeLocalityCluster', {
-        vpc: new sst.aws.Vpc('ServiceScrapePipelineVpc'),
-    }),
+    cluster: new sst.aws.Cluster('ScrapeLocalityCluster', { vpc: Vpc }),
     architecture: 'arm64',
     cpu: '1 vCPU',
     memory: '2 GB',
@@ -42,9 +101,7 @@ const FunctionScrapeLocalityTrigger = new sst.aws.Function('FunctionScrapeLocali
     memory: '1769 MB',
     timeout: '5 seconds',
     concurrency: { reserved: 1 },
-    environment: {
-        ...OTEL_ENV,
-    },
+    environment: { ...OTEL_ENV },
     link: [ScrapeLocalityTask],
     url: $app.stage !== 'production',
 })
