61c85f774fae7512389f1c10a497e11e69e4bc77
Meng Lin <menglinmaker@gmail.com>
2025-12-14 23:43:34 +1100

    remove slice localities

--- apps/service-scrape/function-scrape_locality_trigger/src/index.ts
+++ apps/service-scrape/function-scrape_locality_trigger/src/index.ts
@@ -18,15 +18,12 @@ const batchClient = new BatchClient()
 export const handler = middy().handler(async (_event, _context) => {
     const functionHandlerLogger = new FunctionHandlerLogger(LOGGER)
 
-    const filteredLocality = australiaLocalities
-        .filter((locality) => {
-            if (!locality.postcode) return false
-            if (excludeLocalitySet.has(JSON.stringify(locality))) return false
-            const postcode = parseInt(locality.postcode, 10)
-            return metroPostcode.VIC.includes(postcode)
-        })
-        // TODO: remove temp test code
-        .slice(5)
+    const filteredLocality = australiaLocalities.filter((locality) => {
+        if (!locality.postcode) return false
+        if (excludeLocalitySet.has(JSON.stringify(locality))) return false
+        const postcode = parseInt(locality.postcode, 10)
+        return metroPostcode.VIC.includes(postcode)
+    })
 
     for (const locality of filteredLocality) {
         const newLocality = {
