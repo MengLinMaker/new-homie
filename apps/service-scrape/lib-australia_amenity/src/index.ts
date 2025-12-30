02b49f6640eb16098998165c31a0e95096db1088
Meng Lin <menglinmaker@gmail.com>
2025-12-30 22:42:34 +1100

    add target locality

--- apps/service-scrape/lib-australia_amenity/src/index.ts
+++ apps/service-scrape/lib-australia_amenity/src/index.ts
@@ -1,6 +1,7 @@
 import _australiaSchools from './resource/australia-schools.json' with { type: 'json' }
 import _australiaLocalities from './resource/australia-localities.json' with { type: 'json' }
-import type { Selectable, Updateable } from 'kysely'
+import _targetLocalities from './resource/target-localities.json' with { type: 'json' }
+import type { Updateable } from 'kysely'
 import type { SchemaWrite } from '@service-scrape/lib-db_service_scrape'
 import z from 'zod'
 import { toUpperCaseWords } from './util.ts'
@@ -14,13 +15,11 @@ export const localitySchema = z.object({
 
 export interface Locality extends z.output<typeof localitySchema> {}
 
-export const australiaLocalities = _australiaLocalities as Omit<
-    Selectable<SchemaWrite.LocalityTable>,
-    'boundary_coordinates' | 'id'
->[]
+export const australiaLocalities = _australiaLocalities as Locality[]
+export const targetLocalities = _targetLocalities as Locality[]
 
 export const australiaSchools = _australiaSchools as {
     school_table: Updateable<SchemaWrite.SchoolTable>
     school_feature_table: Updateable<SchemaWrite.SchoolFeatureTable>
-    locality_table: Updateable<SchemaWrite.LocalityTable>
+    locality_table: Locality
 }[]
