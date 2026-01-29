5385930b2ed26b2fedcaddcc4c831bb42234e61c
Meng Lin <menglinmaker@gmail.com>
2026-01-29 23:56:12 +1100

    kill asap

--- apps/service-scrape/function-scrape_locality/src/index.ts
+++ apps/service-scrape/function-scrape_locality/src/index.ts
@@ -7,12 +7,12 @@ import { localitySchema } from '@service-scrape/lib-australia_amenity'
 import { parseEnvSchema } from '@observability/lib-opentelemetry'
 import { browserService } from './global/setup'
 import { handler } from './handler'
-import { exit } from 'node:process'
+import { exit, kill } from 'node:process'
 
 const ENV = await parseEnvSchema(
     z.object({
         LOCALITIES: z.string(),
-        NODE_OPTIONS: z.string().optional(),
+        MANAGED_BY_AWS: z.string().optional(),
     }),
 )
 
@@ -24,11 +24,14 @@ const limit = pLimit(1)
 const jobs = localities.map((loc) => limit(() => handler(loc)))
 await Promise.all(jobs)
 
-// Do not close browser in container - unnecessary operation
-if (!ENV.NODE_OPTIONS) await browserService.close()
-
 const nodeDurationSec = Math.ceil(0.001 * (performance.now() - now))
 console.info(new Date().toISOString(), 'END scrape node - duration sec:', nodeDurationSec)
 
-// Explicitly exit process ASAP
-exit(0)
+// Exit as ASAP in AWS
+if (ENV.MANAGED_BY_AWS !== undefined) {
+    kill(1, 'SIGTERM')
+    exit(0)
+}
+
+await browserService.close()
+
